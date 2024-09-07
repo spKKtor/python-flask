d0a18b169a8eb13812f97a7e7327ff898533745f
spKKtor <stasiik0710@gmail.com>
2024-09-07 11:15:42 +0300

    Add new file

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return "<h1>Моя сторінка на Flask</h1>"
     return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 
 @app.route('/about')
 def about():
-    return "<h2>Тaнк 2 світової війни</h2><p> т-34 рубив Тігрів як дрова</p>"
+    return render_template('about.html', title='About')
 
 
-# only local
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
+
+# Лише для локаотного сервера (закоментувати)
 if __name__ == '__main__':
     app.run(debug=True)
